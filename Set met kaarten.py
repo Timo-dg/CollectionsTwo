de89602dc2613bb8aeebadf4334407cfaacffb68
Timo de Gruiter <timo@EDGITNEW.local>
2021-11-17 11:42:41 +0100

    opdracht afgerond

---
+++ Set met kaarten.py
@@ -0,0 +1,5 @@
+import random
+deck = ['harten 1', 'harten 2', 'harten 3', 'harten 4', 'harten 5', 'harten 6', 'harten 7', 'harten 8', 'harten 9', 'harten 10', 'harten boer', 'harten voruw', 'harten heer', 'harten aas', 'klaveren 1', 'klaveren 2', 'klaveren 3', 'klaveren 4', 'klaveren 5', 'klaveren 6', 'klaveren 7', 'klaveren 8', 'klaveren 9', 'klaveren 10', 'klaveren boer', 'klaveren voruw', 'klaveren heer', 'klaveren aas', 'schoppen 1', 'schoppen 2', 'schoppen 3', 'schoppen 4', 'schoppen 5', 'schoppen 6', 'schoppen 7', 'schoppen 8', 'schoppen 9', 'schoppen 10', 'schoppen boer', 'schoppen voruw', 'schoppen heer', 'schoppen aas', 'ruiten 1', 'ruiten 2', 'ruiten 3', 'ruiten 4', 'ruiten 5', 'ruiten 6', 'ruiten 7', 'ruiten 8', 'ruiten 9', 'ruiten 10', 'ruiten boer', 'ruiten voruw', 'ruiten heer', 'ruiten aas', 'joker', 'joker']
+for x in range(1,8):
+    print('kaart ' + str( x ) + str(random.choices(deck)))
+print('\ndeck ' + str(deck))
